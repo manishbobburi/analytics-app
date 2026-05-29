1cc0e3f00573808b71fd981ab9b98ce862465364
MANISH6148 <manishbobburi6148@gmail.com>
2026-05-29 22:05:43 +0530

    chore: add husky + prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier'
+  ],
+  ignorePatterns: ['dist', 'node_modules', 'vite.config.ts'],
+  overrides: [
+    {
+      files: ['packages/sdk/**/*.ts'],
+      env: { browser: true, es2022: true },
+      globals: {
+        window: 'readonly',
+        document: 'readonly'
+      }
+    },
+    {
+      files: ['packages/api/**/*.ts'],
+      env: { node: true, es2022: true }
+    }
+  ]
+};
