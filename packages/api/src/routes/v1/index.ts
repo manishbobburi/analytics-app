f3cdbb9314b226036171b407cffbea15f00a7ccb
MANISH6148 <manishbobburi6148@gmail.com>
2026-06-24 20:37:20 +0530

    chore(api): remove unused track route file

--- packages/api/src/routes/v1/index.ts
+++ packages/api/src/routes/v1/index.ts
@@ -1,8 +1,4 @@
 import express, { Router } from 'express';
 const router: Router = express.Router();
 
-import trackRouter from './track.routes.js';
-
-router.use('/tracker', trackRouter);
-
 export default router;
