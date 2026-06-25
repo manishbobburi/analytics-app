12cb9d69e1fcee009e8fee27a07469815f5d3bd6
MANISH6148 <manishbobburi6148@gmail.com>
2026-06-25 09:42:29 +0530

    feat(db): add organization repository

---
+++ packages/db/src/repositories/organization.repository.ts
@@ -0,0 +1,8 @@
+import { prisma } from '../client.js';
+import { BaseRepository } from './base.repository.js';
+
+export class OrganizationRepository extends BaseRepository<any> {
+  constructor() {
+    super(prisma.organization);
+  }
+}
