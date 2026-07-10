8e6a90dd6778d112502e5c5af72647d04219eb6e
MANISH6148 <manishbobburi6148@gmail.com>
2026-07-10 23:33:46 +0530

    feat(db): add findByHash method in WriteKeyRepository class

--- packages/db/src/repositories/writeKey.repository.ts
+++ packages/db/src/repositories/writeKey.repository.ts
@@ -5,6 +5,14 @@ class WriteKeyRepository extends BaseRepository<any> {
   constructor() {
     super(prisma.writeKey);
   }
+
+  async findByHash(keyHash: string) {
+    return await prisma.writeKey.findUnique({
+      where: {
+        keyHash: keyHash,
+      },
+    });
+  }
 }
 
 export const writeKeyRepository = new WriteKeyRepository();
