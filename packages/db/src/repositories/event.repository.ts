bdd1e4c64f8616b2102741623a752d0cd7c25d2e
MANISH6148 <manishbobburi6148@gmail.com>
2026-07-11 17:29:41 +0530

    feat(db): add method createMany to support batch ingestion

--- packages/db/src/repositories/event.repository.ts
+++ packages/db/src/repositories/event.repository.ts
@@ -1,9 +1,23 @@
-import { prisma } from '../client.js';
+import { NormalizedEvent } from '@app/shared';
+import { prisma, Prisma } from '../client.js';
 import { BaseRepository } from './base.repository.js';
 
 class EventRepository extends BaseRepository<any> {
   constructor() {
     super(prisma.event);
+  }
+
+  async createMany(normalizedEvents: NormalizedEvent[]) {
+    const data = normalizedEvents.map((event) => ({
+      ...event,
+      properties: event.properties as Prisma.InputJsonValue,
+      context: event.context as Prisma.InputJsonValue,
+    }));
+
+    return prisma.event.createMany({
+      data: data,
+      skipDuplicates: true,
+    });
   }
 }
 
