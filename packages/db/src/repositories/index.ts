5eff41d7606e911a7f329d1a88bf3aaa0012db4f
MANISH6148 <manishbobburi6148@gmail.com>
2026-07-18 00:20:58 +0530

    feat(db): add class RefreshTokenRepository

--- packages/db/src/repositories/index.ts
+++ packages/db/src/repositories/index.ts
@@ -1,3 +1,4 @@
 export * from './organization.repository.js';
 export * from './writeKey.repository.js';
 export * from './event.repository.js';
+export * from './refreshToken.repository.js';
---
+++ packages/db/src/repositories/refreshToken.repository.ts
@@ -0,0 +1,10 @@
+import { prisma } from '../client.js';
+import { BaseRepository } from './base.repository.js';
+
+class RefreshTokenRepository extends BaseRepository<any> {
+  constructor() {
+    super(prisma.refreshToken);
+  }
+}
+
+export const refreshTokenRepository = new RefreshTokenRepository();
