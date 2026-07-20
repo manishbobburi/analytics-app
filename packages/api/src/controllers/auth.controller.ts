1c887eb3c3374385993b723589256985848c3c82
MANISH6148 <manishbobburi6148@gmail.com>
2026-07-20 21:16:53 +0530

    feat(api): implement refresh access token controller

--- packages/api/src/controllers/auth.controller.ts
+++ packages/api/src/controllers/auth.controller.ts
@@ -4,6 +4,7 @@ import { authService } from '../services/index.js';
 import successResponse from '../utils/common/success-response.js';
 import { serverConfig } from '../config/index.js';
 import type { LoginInput } from '../types/auth.types.js';
+import { AppError } from '../error/index.js';
 
 async function login(req: Request, res: Response) {
   const payload: LoginInput = {
@@ -24,4 +25,28 @@ async function login(req: Request, res: Response) {
   return res.status(StatusCodes.OK).json(successResponse({ accessToken }));
 }
 
-export { login };
+async function refreshAccessToken(req: Request, res: Response) {
+  const token = req.cookies.refreshToken;
+
+  if (!token) {
+    throw new AppError(
+      'Refresh token missing.',
+      StatusCodes.UNAUTHORIZED,
+      'REFRESH_TOKEN_NOT_FOUND'
+    );
+  }
+
+  const { accessToken, refreshToken } = await authService.refreshAccessToken(token);
+
+  res.cookie('refreshToken', refreshToken.token, {
+    httpOnly: true,
+    secure: serverConfig.isProd,
+    sameSite: 'strict',
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    path: '/auth/refresh',
+  });
+
+  return res.status(StatusCodes.OK).json(successResponse({ accessToken }));
+}
+
+export { login, refreshAccessToken };
