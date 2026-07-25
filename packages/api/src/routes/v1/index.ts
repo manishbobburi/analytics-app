37391f1cbe5fd0e7cdf93347b2c34b9b11650bae
MANISH6148 <manishbobburi6148@gmail.com>
2026-07-25 10:58:29 +0530

    feat(routes): add event routes

--- packages/api/src/routes/v1/index.ts
+++ packages/api/src/routes/v1/index.ts
@@ -1,4 +1,14 @@
 import express, { Router } from 'express';
 const router: Router = express.Router();
 
+import eventRouter from './event.routes.js';
+import writeKeyRouter from './writeKey.routes.js';
+import authRouter from './auth.routes.js';
+
+router.use('/events', eventRouter);
+
+router.use('/writeKey', writeKeyRouter);
+
+router.use('/auth', authRouter);
+
 export default router;
