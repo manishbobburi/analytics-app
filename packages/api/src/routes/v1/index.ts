de9acdb0b4a5d1ca1705e113c1a73c8dcd9b6315
MANISH6148 <manishbobburi6148@gmail.com>
2026-07-26 22:49:10 +0530

    feat(routes): add analytics routes

--- packages/api/src/routes/v1/index.ts
+++ packages/api/src/routes/v1/index.ts
@@ -4,11 +4,14 @@ const router: Router = express.Router();
 import eventRouter from './event.routes.js';
 import writeKeyRouter from './writeKey.routes.js';
 import authRouter from './auth.routes.js';
+import analyticsRouter from './analytics.routes.js';
 
 router.use('/events', eventRouter);
 
 router.use('/writeKey', writeKeyRouter);
 
 router.use('/auth', authRouter);
+
+router.use('/analytics', analyticsRouter);
 
 export default router;
