6de2f98c482180684742da66c215dd68095c9d89
MANISH6148 <manishbobburi6148@gmail.com>
2026-08-17 17:36:41 +0530

    refactor(analytics): change card description of breakdown chart

--- apps/dashboard/src/features/analytics/components/Breakdown.tsx
+++ apps/dashboard/src/features/analytics/components/Breakdown.tsx
@@ -51,7 +51,7 @@ export function Breakdown() {
       <CardHeader className="flex flex-row items-center justify-between">
         <div className="flex flex-1 flex-col justify-center gap-1 px-6 pb-3 sm:pb-0">
           <CardTitle>Breakdown</CardTitle>
-          <CardDescription>January - June 2024</CardDescription>
+          <CardDescription>Events by selected dimension</CardDescription>
         </div>
 
         <DimensionSelect value={dimension} onChange={setDimension} />
